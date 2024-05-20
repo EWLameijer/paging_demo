05cde4f337f16efbd7e8a7226de183208f82f9ff
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-20 13:30:17 +0200

    Initial commit

---
+++ src/main/java/org/ericwubbo/pagingdemo/PagingDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.pagingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ericwubbo/pagingdemo/PagingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.pagingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
