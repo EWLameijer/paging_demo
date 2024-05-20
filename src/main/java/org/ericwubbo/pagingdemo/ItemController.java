c76ee938f6792604553aeedcc21b54d301e24130
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-20 17:55:21 +0200

    Make it updateable

--- src/main/java/org/ericwubbo/pagingdemo/ItemController.java
+++ src/main/java/org/ericwubbo/pagingdemo/ItemController.java
@@ -3,9 +3,11 @@ package org.ericwubbo.pagingdemo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.data.domain.Sort.Order.desc;
+import static org.springframework.data.domain.Sort.by;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,6 @@ public class ItemController {
         return itemRepository.findAll(PageRequest.of(
                 pageable.getPageNumber(),
                 Math.min(pageable.getPageSize(), 3),
-                Sort.by(Sort.Order.desc("price"))));
+                pageable.getSortOr(by(desc("price")))));
     }
 }
