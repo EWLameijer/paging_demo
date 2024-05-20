252626647708ed644517ca6f3ed93ad05fad6a6f
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-20 13:32:44 +0200

    removed unused imports

--- src/main/java/org/ericwubbo/pagingdemo/ItemController.java
+++ src/main/java/org/ericwubbo/pagingdemo/ItemController.java
@@ -1,12 +1,8 @@
 package org.ericwubbo.pagingdemo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.math.BigDecimal;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
